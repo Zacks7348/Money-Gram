34bcd2728ed4614c67bd3e08be5feb247e72b99b
sequeirajonathan <sequeira.s.jonathan@gmail.com>
2021-04-08 20:09:08 -0400

    Fixed pagination

--- client/src/components/StatementsComponent/StatementsComponent.jsx
+++ client/src/components/StatementsComponent/StatementsComponent.jsx
@@ -9,6 +9,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import TablePagination from "@material-ui/core/TablePagination";
 
 
 // Recoil
@@ -18,6 +19,7 @@ import { responseUserIDState } from '../../Store/Atoms'
 const useStyles = makeStyles({
     table: {
         minWidth: 650,
+        marginTop: '3vh'
     },
 });
 
@@ -27,6 +29,19 @@ export default function StatementsTable() {
     const responseUserID = useRecoilValue(responseUserIDState);
 
     const [data, setData] = useState([{}]);
+    
+    const [page, setPage] = React.useState(0);
+    
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+     const handleChangePage = (event, newPage) => {
+       setPage(newPage);
+     };
+
+     const handleChangeRowsPerPage = (event) => {
+       setRowsPerPage(+event.target.value);
+       setPage(0);
+     };
 
     useEffect(() => {
         if (!responseUserID) {
@@ -42,37 +57,55 @@ export default function StatementsTable() {
 
     
 
-
     return (
+      <>
         <TableContainer component={Paper}>
-            <Table className={classes.table} size="small" aria-label="a dense table">
-                <TableHead>
-                    <TableRow key={Math.random().toString(36).substr(2, 9)} >
-                        <TableCell >Transaction ID</TableCell>
-                        <TableCell align="right">Transaction Date</TableCell>
-                        <TableCell align="right">Reciever</TableCell>
-                        <TableCell align="right">Amount&nbsp;(USD)</TableCell>
-                        <TableCell align="right">Memo</TableCell>
-                        <TableCell align="right">Transaction Status</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        data.map((statement) => {
-                            return (
-                                <TableRow key={Math.random().toString(36).substr(2, 9)} >
-                                    <TableCell>{statement.transaction_id}</TableCell>
-                                    <TableCell align="right">{moment(statement.date).format("MMM Do YYYY")}</TableCell>
-                                    <TableCell align="right">{statement.reciever_username}</TableCell>
-                                    <TableCell align="right">{"$" + statement.amount}</TableCell>
-                                    <TableCell align="right">{statement.memo === null ? "N/A" : statement.memo}</TableCell>
-                                    <TableCell align="right">{statement.status_description}</TableCell>
-                                </TableRow>
-                            )
-                        })
-                    }
-                </TableBody>
-            </Table>
+          <Table className={classes.table} aria-label="Statement Table">
+            <TableHead>
+              <TableRow key={Math.random().toString(36).substr(2, 9)}>
+                <TableCell>Transaction ID</TableCell>
+                <TableCell align="right">Transaction Date</TableCell>
+                <TableCell align="right">Reciever</TableCell>
+                <TableCell align="right">Amount&nbsp;(USD)</TableCell>
+                <TableCell align="right">Memo</TableCell>
+                <TableCell align="right">Transaction Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((statement) => {
+                return (
+                  <TableRow key={Math.random().toString(36).substr(2, 9)}>
+                    <TableCell>{statement.transaction_id}</TableCell>
+                    <TableCell align="right">
+                      {moment(statement.date).format("MMM Do YYYY")}
+                    </TableCell>
+                    <TableCell align="right">
+                      {statement.reciever_username}
+                    </TableCell>
+                    <TableCell align="right">
+                      {"$" + statement.amount}
+                    </TableCell>
+                    <TableCell align="right">
+                      {statement.memo === null ? "N/A" : statement.memo}
+                    </TableCell>
+                    <TableCell align="right">
+                      {statement.status_description}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
         </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[1,5,10]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </>
     );
 }
